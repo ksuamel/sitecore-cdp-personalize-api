c8793c7f3529a6cbb7d9337ac05ee94048d94a72
Kevin Suarez <kevin.suarez.melendez@gmail.com>
2022-10-07 12:03:29 -0400

    Upsert now combines data from upstream

--- src/guest/guest-data-extensions-api.ts
+++ src/guest/guest-data-extensions-api.ts
@@ -1,27 +1,89 @@
 import { CdpHttpClient } from '../base/cdp-http-client';
 import { CdpConfiguration } from '../types';
 
+type LocateReponse = {
+  href: string;
+  offset: number;
+  limit: number;
+  items: Array<{ href: string }>;
+};
+
 export class GuestDataExtensionsApi extends CdpHttpClient {
   constructor(configuration: CdpConfiguration) {
     super(configuration);
   }
 
-  locate = (guestRef: string, extensionName: string): Promise<unknown> => {
-    return this.get(`guests/${guestRef}/${extensionName}`);
+  locate = (
+    guestRef: string,
+    extensionName: string
+  ): Promise<LocateReponse> => {
+    return this.get(`guests/${guestRef}/${extensionName}`) as Promise<
+      LocateReponse
+    >;
   };
+
   retrieve = (
     guestRef: string,
     extensionName: string,
     extensionId: string
-  ): Promise<unknown> => {
+  ): Promise<any> => {
     return this.get(`guests/${guestRef}/${extensionName}/${extensionId}`);
   };
-  upsert = (
+
+  create = (
     guestRef: string,
     extensionName: string,
     body: Record<string, unknown>
   ): Promise<unknown> => {
     return this.post(`guests/${guestRef}/${extensionName}`, body);
+  };
+
+  update = (
+    guestRef: string,
+    extensionName: string,
+    extensionId: string,
+    body: Record<string, unknown>
+  ): Promise<unknown> => {
+    return this.post(
+      `guests/${guestRef}/${extensionName}/${extensionId}`,
+      body
+    );
+  };
+
+  upsert = async (
+    guestRef: string,
+    extensionName: string,
+    body: Record<string, unknown>
+  ): Promise<unknown> => {
+    const extensions = await this.locate(guestRef, extensionName);
+    if (extensions.items.length == 0) {
+      return this.post(`guests/${guestRef}/${extensionName}`, body);
+    }
+
+    const extensionId = this.getUrlLastSegment(extensions.items[0].href);
+    const extensionData = await this.retrieve(
+      guestRef,
+      extensionName,
+      extensionId
+    );
+    if (extensionData == null) {
+      throw 'Failed to retrieve extension data';
+    }
+
+    const {
+      href,
+      ref,
+      key,
+      createdAt,
+      modifiedAt,
+      guest,
+      ...customData
+    } = extensionData;
+
+    return this.post(`guests/${guestRef}/${extensionName}/${extensionId}`, {
+      ...customData,
+      ...body,
+    });
   };
   delete = (
     guestRef: string,
@@ -29,5 +91,9 @@ export class GuestDataExtensionsApi extends CdpHttpClient {
     extensionId: string
   ): Promise<unknown> => {
     return this.del(`guests/${guestRef}/${extensionName}/${extensionId}`);
+  };
+
+  private getUrlLastSegment = (url: string) => {
+    return url.substring(url.lastIndexOf('/') + 1);
   };
 }
